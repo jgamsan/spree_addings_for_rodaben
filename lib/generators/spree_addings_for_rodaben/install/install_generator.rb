e2791b97443c280fbc79867c455a55bbfd28bbb9
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-07-24 16:50:15 +0200

    Added csv files

--- lib/generators/spree_addings_for_rodaben/install/install_generator.rb
+++ lib/generators/spree_addings_for_rodaben/install/install_generator.rb
@@ -11,7 +11,17 @@ module SpreeAddingsForRodaben
         inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_addings_for_rodaben\n", :before => /\*\//, :verbose => true
         inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_addings_for_rodaben\n", :before => /\*\//, :verbose => true
       end
-
+      
+      def add_datas
+        run 'mkdir db/datas' unless Dir.exist?('db/datas')
+        copy_file "anchos.csv", "db/datas/rodaben-anchos.csv"
+        copy_file "llantas.csv", "db/datas/rodaben-llantas.csv"
+        copy_file "perfiles.csv", "db/datas/rodaben-series.csv"
+        copy_file "vel.csv", "db/datas/rodaben-ivel.csv"
+        copy_file "marcas.csv", "db/datas/rodaben-marcas.csv"
+        copy_file "taxons.csv", "db/datas/rodaben-taxons.csv"
+      end
+      
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_addings_for_rodaben'
       end
