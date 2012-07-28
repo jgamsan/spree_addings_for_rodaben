b8f05f58847e0960bd2d4e6dde48f04518cd96bb
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-07-28 08:37:38 +0200

    Added more fields to search

--- db/migrate/20120728061409_modified_several_fields.rb
+++ db/migrate/20120728061409_modified_several_fields.rb
@@ -1,11 +1,12 @@
 class ModifiedSeveralFields < ActiveRecord::Migration
   def up
-    change_column :spree_variants, :tire_gr, :boolean
-    change_column :spree_variants, :tire_rf, :boolean
+    remove_column :spree_variants, :tire_gr
+    remove_column :spree_variants, :tire_rf
+    add_column :spree_variants, :tire_gr, :boolean
+    add_column :spree_variants, :tire_rf, :boolean
   end
 
   def down
-    change_column :spree_variants, :tire_gr, :integer
-    change_column :spree_variants, :tire_rf, :integer
+    
   end
 end
