5ba6be1a0ebbf4bbb8e1b2bd1e7823b412b05ce7
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-08-04 08:42:29 +0200

    Mejoras en csv

--- app/overrides/add_tab_for_rodaben_admin.rb
+++ app/overrides/add_tab_for_rodaben_admin.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_tabs",
+Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "add_tab_for_rodaben_admin",
-                     :insert_bottom => nil,
+                     :insert_bottom => "[data-hook='admin_tabs']",
                      :text => "<%= tab :tires_admin, :label => 'Gestion Neumaticos' %>")
