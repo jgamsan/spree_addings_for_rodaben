61562604781c0af8274c451af796602ba1a59118
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-08-04 09:52:52 +0200

    Create controller for tires

--- app/controllers/spree/admin/admin_tires_controller.rb
+++
@@ -1,7 +0,0 @@
-module Spree
-  module Admin
-    class AdminTiresController < Spree::Admin::BaseController
-
-    end
-  end
-end
--- app/controllers/spree/admin/tires_admin_controller.rb
+++
@@ -1,7 +0,0 @@
-module Spree
-  module Admin
-    class TiresAdminController < Spree::Admin::BaseController
-
-    end
-  end
-end
