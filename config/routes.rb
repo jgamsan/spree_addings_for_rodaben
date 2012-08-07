597d95c4b2c0fde2141986345ecca9d653484e1c
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-08-07 07:54:27 +0200

    mejora en gestion neumaticos

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
-    resource :tire_settings, :only => :index
-    resource :tire_widths
-    resource :tire_serials
-    resource :tire_innertubes
-    resource :tire_speed_codes
+    resources :tire_settings, :only => :index
+    resources :tire_widths
+    resources :tire_serials
+    resources :tire_innertubes
+    resources :tire_speed_codes
   end
 end
