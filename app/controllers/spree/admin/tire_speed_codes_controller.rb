221d430ad5131a214212d1797325e0b95a0b38ac
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-08-08 08:54:15 +0200

    Corregido error en edit action

--- app/controllers/spree/admin/tire_speed_codes_controller.rb
+++ app/controllers/spree/admin/tire_speed_codes_controller.rb
@@ -1,55 +1,11 @@
 module Spree
   module Admin
-    class TireSpeedCodesController < Spree::Admin::BaseController
+    class TireSpeedCodesController < ResourceController
+      
       def index
-        @speed_codes = Spree::TireSpeedCode.order('name').page(params[:page]).per(10)
+        @tire_speed_codes = Spree::TireSpeedCode.order('name').page(params[:page]).per(10)
       end
       
-      def new
-        @speed_code = Spree::TireSpeedCode.new
-      end
-      
-      def edit
-        @speed_code = Spree::TireSpeedCode.find(params[:id])      
-      end
-      
-      def create
-        @speed_code = Spree::TireSpeedCode.new(params[:speed_code])
-
-        respond_to do |format|
-          if @speed_code.save
-            format.html { redirect_to(admin_tire_settings_path, :notice => 'Nuevo Codigo Velocidad creado correctamente.') }
-            format.xml  { render :xml => @speed_code, :status => :created, :location => @speed_code }
-          else
-            format.html { render :action => "new" }
-            format.xml  { render :xml => @speed_code.errors, :status => :unprocessable_entity }
-          end
-        end
-      end
-      
-      def update
-        @speed_code = Spree::TireSpeedCode.find(params[:id])
-
-        respond_to do |format|
-          if @speed_code.update_attributes(params[:speed_code])
-            format.html { redirect_to(admin_tire_settings_path, :notice => 'Codigo Velocidad actualizado.') }
-            format.xml  { head :ok }
-          else
-            format.html { render :action => "edit" }
-            format.xml  { render :xml => @speed_code.errors, :status => :unprocessable_entity }
-          end
-        end
-      end
-      
-      def delete
-        @speed_code = Spree::TireSpeedCode.find(params[:id])
-        @speed_code.destroy
-
-        respond_to do |format|
-          format.html { redirect_to(admin_tire_settings_path) }
-          format.xml  { head :ok }
-        end
-      end
     end
   end
 end 
--- app/controllers/spree/admin/tire_widths_controller.rb
+++ app/controllers/spree/admin/tire_widths_controller.rb
@@ -5,53 +5,7 @@ module Spree
       def index
         @tire_widths = Spree::TireWidth.order("name").page(params[:page]).per(10)
       end
-=begin      
-      def new
-        @width = Spree::TireWidth.new
-      end
-      
-      def edit
-        @width = Spree::TireWidth.find(params[:id])      
-      end
-      
-      def create
-        @width = Spree::TireWidth.new(params[:width])
-
-        respond_to do |format|
-          if @width.save
-            format.html { redirect_to(admin_tire_settings_path, :notice => 'Nuevo Ancho creado correctamente.') }
-            format.xml  { render :xml => @width, :status => :created, :location => @width }
-          else
-            format.html { render :action => "new" }
-            format.xml  { render :xml => @width.errors, :status => :unprocessable_entity }
-          end
-        end
-      end
-      
-      def update
-        @width = Spree::TireWidth.find(params[:id])
-
-        respond_to do |format|
-          if @width.update_attributes(params[:width])
-            format.html { redirect_to(admin_tire_settings_path, :notice => 'Ancho actualizado.') }
-            format.xml  { head :ok }
-          else
-            format.html { render :action => "edit" }
-            format.xml  { render :xml => @width.errors, :status => :unprocessable_entity }
-          end
-        end
-      end
-      
-      def delete
-        @width = Spree::TireWidth.find(params[:id])
-        @width.destroy
-
-        respond_to do |format|
-          format.html { redirect_to(admin_tire_settings_path) }
-          format.xml  { head :ok }
-        end
-      end
-=end
+    
     end
   end
 end 
