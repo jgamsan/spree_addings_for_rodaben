42b4af3a8bd1ca8435829c2701f0d3cccfdf29d1
jose miguel <miguel.gamazo@galiclick.com>
2012-08-19 18:41:39 +0200

    Mejoras en mailer

--- lib/spree_addings_for_rodaben/engine.rb
+++ lib/spree_addings_for_rodaben/engine.rb
@@ -15,55 +15,6 @@ module SpreeAddingsForRodaben
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
-      Spree::Order.class_eval do
-        Spree::Order.state_machines[:state] = StateMachine::Machine.new(Order, :initial => 'cart') do
-          event :next do
-            transition :from => 'cart',     :to => 'address'
-            transition :from => 'address',  :to => 'delivery'
-            transition :from => 'delivery', :to => 'payment', :if => :payment_required?
-            transition :from => 'delivery', :to => 'complete'
-            transition :from => 'confirm',  :to => 'complete'
-
-            # note: some payment methods will not support a confirm step
-            transition :from => 'payment',  :to => 'confirm',
-                                            :if => Proc.new { |order| order.payment_method && order.payment_method.payment_profiles_supported? }
-
-            transition :from => 'payment', :to => 'complete'
-          end
-
-          event :cancel do
-            transition :to => 'canceled', :if => :allow_cancel?
-          end
-          event :return do
-            transition :to => 'returned', :from => 'awaiting_return'
-          end
-          event :resume do
-            transition :to => 'resumed', :from => 'canceled', :if => :allow_resume?
-          end
-          event :authorize_return do
-            transition :to => 'awaiting_return'
-          end
-
-          before_transition :to => 'complete' do |order|
-            begin
-              order.process_payments!
-            rescue Core::GatewayError
-              !!Spree::Config[:allow_checkout_on_gateway_error]
-            end
-          end
-
-          before_transition :to => ['delivery'] do |order|
-            order.shipments.each { |s| s.destroy unless s.shipping_method.available_to_order?(order) }
-          end
-          around_transition :to => 'complete', :do => :deliver_order_company_provider_email
-          after_transition :to => 'complete', :do => :finalize!
-          after_transition :to => 'delivery', :do => :create_tax_charge!
-          after_transition :to => 'payment',  :do => :create_shipment!
-          after_transition :to => 'resumed',  :do => :after_resume
-          after_transition :to => 'canceled', :do => :after_cancel
-
-        end
-      end
     end
 
     config.to_prepare &method(:activate).to_proc
