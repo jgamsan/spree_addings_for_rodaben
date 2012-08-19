dc1260dac364378d26d3a26b1640ed61581c6df0
jose miguel <miguel.gamazo@galiclick.com>
2012-08-19 16:51:37 +0200

    Mejoras en emails

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,8 +1,19 @@
 Spree::Order.class_eval do
 
   def finalize!
-    super
-    deliver_order_company_provider_email
+    touch :completed_at
+      InventoryUnit.assign_opening_inventory(self)
+      # lock any optional adjustments (coupon promotions, etc.)
+      adjustments.optional.each { |adjustment| adjustment.update_column('locked', true) }
+      deliver_order_confirmation_email
+      deliver_order_company_provider_email
+      self.state_changes.create({
+        :previous_state => 'cart',
+        :next_state     => 'complete',
+        :name           => 'order' ,
+        :user_id        => (User.respond_to?(:current) && User.current.try(:id)) || self.user_id
+      }, :without_protection => true)
+
   end
 
   def deliver_order_company_provider_email
