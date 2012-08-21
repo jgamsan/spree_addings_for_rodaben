3e2a4b954b701136a0002b9b95c686350f0651a5
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-08-21 12:18:10 +0200

    Mejoras en prices

---
+++ app/overrides/replace_price_in_products.rb
@@ -0,0 +1,19 @@
+Deface::Override.new(:virtual_path => %q{spree/shared/_products},
+                     :name => %q{replace_price_in_products},
+                     :replace => %q{span.price},
+                     :text => %q{
+      <% if (@product.price - @product.price_in_offert) < 0 %>
+        <div>
+          <span class="price selling" style="color:red">
+            <%= number_to_currency @product.price %></span>
+          <span class="price selling" itemprop="price" style="text-decoration: line-through;">
+            <%= number_to_currency @product.price_in_offert %>
+          </span>
+        </div>
+      <% else %>
+        <div>
+          <span class="price selling" itemprop="price">
+            <%= number_to_currency @product.price %>
+          </span>
+        </div>
+      <% end %>})
