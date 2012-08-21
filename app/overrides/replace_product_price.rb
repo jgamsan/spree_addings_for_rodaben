44d27af9333cfa267ed75d9e8c4a98962e22a598
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-08-21 11:16:36 +0200

    Mejoras en prices

--- app/overrides/replace_product_price.rb
+++ app/overrides/replace_product_price.rb
@@ -1,6 +1,6 @@
-Deface::Override.new(:virtual_path => %q{spree/products/_cart},
+Deface::Override.new(:virtual_path => %q{spree/products/_cart_form},
                      :name => %q{replace_product_price},
-                     :replace => %q{div#product-price},
+                     :replace => %q{#product-price},
                      :text => %q{
       <h6 class="product-section-title"><%= t(:price) %></h6>
       <% if (@product.price - @product.price_in_offert) < 0 %>
