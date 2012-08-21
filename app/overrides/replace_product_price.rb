fc57674563a5ced5153c47b2e4a0e03b896d43df
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-08-21 12:12:01 +0200

    Mejoras en prices

--- app/overrides/replace_product_price.rb
+++ app/overrides/replace_product_price.rb
@@ -2,12 +2,13 @@ Deface::Override.new(:virtual_path => %q{spree/products/_cart_form},
                      :name => %q{replace_product_price},
                      :replace => %q{#product-price},
                      :text => %q{
+      <div id="product-price">
       <h6 class="product-section-title"><%= t(:price) %></h6>
       <% if (@product.price - @product.price_in_offert) < 0 %>
         <div>
           <span class="price selling" style="color:red">
             <%= number_to_currency @product.price %></span>
-          <span class="price selling" itemprop="price" style="text-decoration: line-through;>
+          <span class="price selling" itemprop="price" style="text-decoration: line-through;">
             <%= number_to_currency @product.price_in_offert %>
           </span>
         </div>
@@ -17,4 +18,5 @@ Deface::Override.new(:virtual_path => %q{spree/products/_cart_form},
             <%= number_to_currency @product.price %>
           </span>
         </div>
-      <% end %>})
+      <% end %>
+      </div>})
