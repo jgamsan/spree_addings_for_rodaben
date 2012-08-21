75aaf6cf968958d8fce7b2e36bae52bdf9d242e3
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-08-21 11:02:12 +0200

    Mejoras en prices

--- app/overrides/add_price_offert_in_admin.rb
+++ app/overrides/add_price_offert_in_admin.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                       :name => "add_price_offert_in_admin",
                       :insert_before => "code[erb-loud]:contains('f.field_container :available_on do')",
-                      %q{
+                      :text => %q{
           <%= f.field_container :price_in_offert do %>
             <%= f.label :price_in_offert, t(:price_in_offert) %><br />
             <%= f.text_field :price_in_offert, :value => number_to_currency(@product.cost_price, :unit => '') %>
