9d919304507f97a23bdc9b7c64a8508a9ad512ec
jose miguel <miguel.gamazo@galiclick.com>
2012-08-23 19:39:50 +0200

    Mas mejoras

--- app/overrides/replace_homepage_products.rb
+++ app/overrides/replace_homepage_products.rb
@@ -3,11 +3,12 @@ Deface::Override.new(:virtual_path => %q{spree/home/index},
                           :replace => %q{[data-hook='homepage_products']},
                           :closing_selector => %q{},
                           :text => %q{
+            <div data-hook="homepage_products">
             <div id="banner" class="row" data-hook="products_banner">
-              <%= render :partial => 'spree/shared/banner', :locals => { :cycle_images => @cycle_images } %>
+              <%= render :partial => "spree/shared/banner", :locals => { :cycle_images => @cycle_images } %>
             </div>
             <div id="offerts" class="row" data-hook="products_in_offert">
               <h3>Listado de Productos en Oferta</h3>
               <%= render :partial => 'spree/shared/products', :locals => { :products => @products } %>
             </div>
-                          })
+            </div>               })
