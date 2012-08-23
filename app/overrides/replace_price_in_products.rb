76573a006ffe4e8a5418a5d9af2564066ead6e98
jose miguel <miguel.gamazo@galiclick.com>
2012-08-23 20:55:27 +0200

    Mas mejoras

--- app/overrides/replace_price_in_products.rb
+++ app/overrides/replace_price_in_products.rb
@@ -3,16 +3,14 @@ Deface::Override.new(:virtual_path => %q{spree/shared/_products},
                      :replace => %q{span.price},
                      :text => %q{
       <% if (product.price - product.price_in_offert) < 0 %>
-        <div>
-          <span class="price selling" style="color:red">
-            <%= number_to_currency product.price %></span>
-            <span class="price selling" itemprop="price" style="text-decoration: line-through;">
-            <%= number_to_currency product.price_in_offert %>
-        </div>
+        <span class="price selling" style="color:red">
+          <%= number_to_currency product.price %></span>
+          <span class="price selling" itemprop="price" style="text-decoration: line-through;">
+          <%= number_to_currency product.price_in_offert %>
       <% else %>
-        <div>
+
           <span class="price selling" itemprop="price">
             <%= number_to_currency product.price %>
           </span>
-        </div>
+
       <% end %>})
