3e1da422d4704efc1b651917ee52fc7e77c0eab3
jose miguel <miguel.gamazo@galiclick.com>
2012-08-27 17:39:06 +0200

    Added supplier in admin

---
+++ app/overrides/add_supplier_to_products_in_admin.rb
@@ -0,0 +1,9 @@
+Deface::Override.new(:virtual_path => "spree/admin/products/_form",
+                      :name => "add_supplier_to_products_in_admin",
+                      :insert_bottom => "data-hook='admin_product_form_left'",
+                      :text => %q{
+          <%= f.field_container :supplier do %>
+        <%= f.label :supplier_id, t(:supplier_id) %><br />
+        <%= f.collection_select(:supplier_id, Spree::Supplier.all, :id, :name, { :include_blank => true }, { 'style' => 'width:200px' }) %>
+        <%= f.error_message_on :supplier_id %>
+      <% end %> %>})
