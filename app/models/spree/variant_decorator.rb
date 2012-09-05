c00203795487212360b5d822373c1bccbfa2a8cb
jose miguel <miguel.gamazo@galiclick.com>
2012-09-05 19:37:56 +0200

    Corregido error en variant class

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,8 +1,8 @@
 Spree::Variant.class_eval do
-  has_many :tire_widths, :class_name => "Spree::TireWidth"
-  has_many :tire_serials, :class_name => "Spree::TireSerial"
-  has_many :tire_innertubes, :class_name => "Spree::TireInnertube"
-  has_many :tire_speed_codes, :class_name => "Spree::TireSpeedCode"
+  has_one :tire_width, :class_name => "Spree::TireWidth"
+  has_one :tire_serial, :class_name => "Spree::TireSerial"
+  has_one :tire_innertube, :class_name => "Spree::TireInnertube"
+  has_one :tire_speed_code, :class_name => "Spree::TireSpeedCode"
 
   attr_accessible :count_on_hand, :cost_price, :price, :price_in_offert
 
