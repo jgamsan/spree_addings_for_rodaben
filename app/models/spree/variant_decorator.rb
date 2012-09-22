bb88dac9c248a806d4f97749eec5d0fbbde33b8d
jose miguel <miguel.gamazo@galiclick.com>
2012-09-22 13:48:46 +0200

    Mejoras en busquedas

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -16,4 +16,25 @@ Spree::Variant.class_eval do
   ["TT", 2],
   ["RU", 3]
   ]
+
+  def existe_tire?(name, width, serial, innertube, speed_code)
+    ancho = Spree::TireWidth.find_by_name(width) unless width.nil?
+    serial = Spree::TireSerial.find_by_name(serial) unless serial.nil?
+    llanta = Spree.TireInnertube.find_by_name(innertube) unless innertube.nil?
+    vel = Spree::TireSpeedCode.find_by_name(speed_code) unless speed_code.nil?
+
+    base = "Select * from spree_variants where name = #{name}"
+    base << " and tire_width_id = #{ancho}" unless ancho.nil?
+    base << " and tire_serial_id = #{serial}" unless serial.nil?
+    base << " and tire_innertube_id = #{llanta}" unless llanta.nil?
+    base << " and tire_speed_code_id = #{vel}" unless vel.nil?
+    base << ";"
+
+    item = Spree::Variant.find_by_sql(base)
+    if item.nil?
+      false
+    else
+      true
+    end
+  end
 end
