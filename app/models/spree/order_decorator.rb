93870d1c1f3e0efe14a2f16679293d0a0bc08ab2
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-10-07 18:17:49 +0200

    Added workshop to checkout

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,5 +1,15 @@
 Spree::Order.class_eval do
 
+  checkout_flow do
+      go_to_state :address
+      go_to_state :delivery
+      go_to_state :workshop
+      go_to_state :payment, :if => lambda { |order| order.payment_required? }
+      go_to_state :confirm, :if => lambda { |order| order.confirmation_required? }
+      go_to_state :complete
+      remove_transition :from => :delivery, :to => :confirm
+    end
+
   def finalize!
       touch :completed_at
       Spree::InventoryUnit.assign_opening_inventory(self)
---
+++ app/overrides/add_workshop_to_checkout.rb
@@ -0,0 +1,10 @@
+Deface::Override.new(:virtual_path => "spree/shared/_order_details",
+                      :name => "add_workshop_to_checkout",
+                      :insert_before => "code[erb-loud]:contains('if @order.has_step?('delivery')')",
+                      :text => %q{
+    <div class="columns alpha four">
+    <h6><%= t(:workshop) %> <%= link_to "(#{t(:edit)})", checkout_state_path(:workshop) unless @order.completed? %></h6>
+    <div class="address">
+      <%= order.workshop %>
+    </div>
+  </div>
