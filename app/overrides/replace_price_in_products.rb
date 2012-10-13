f06ec3e1ff0a7e2f6a28ad9f976670376f8751e5
jose miguel <miguel.gamazo@galiclick.com>
2012-10-13 21:49:47 +0200

    Mas mejoras

--- app/overrides/replace_price_in_products.rb
+++ app/overrides/replace_price_in_products.rb
@@ -1,8 +1,18 @@
 Deface::Override.new(:virtual_path => %q{spree/shared/_products},
-                     :name => %q{replace_price_in_products},
-                     :replace => %q{span.price},
+                     :name => %q{add_tires_info_in_products},
+                     :replace => "ul#products",
                      :text => %q{
-      <% if (product.price - product.price_in_offert) < 0 %>
+   <ul id="products" class="inline product-listing" data-hook>
+  <% reset_cycle('default') %>
+  <% products.each do |product| %>
+    <% if Spree::Config[:show_zero_stock_products] || product.has_stock? %>
+      <li id="product_<%= product.id %>" class="columns three <%= cycle("alpha", "secondary", "", "omega secondary", :name => "classes") %>" data-hook="products_list_item" itemscope itemtype="http://schema.org/Product">
+        <div class="product-image">
+          <%= link_to small_image(product, :itemprop => "image"), product, :itemprop => 'url' %>
+        </div>
+        <%= link_to truncate(product.name, :length => 50), product, :class => 'info', :itemprop => "name", :title => product.name %>
+        <br> <%= product.calculate_tires(product.id) %>
+        <% if (product.price - product.price_in_offert) < 0 %>
         <span class="price selling" style="color:red">
           <%= product.display_price %></span>
           <span class="price selling" itemprop="price" style="text-decoration: line-through;">
@@ -13,11 +23,9 @@ Deface::Override.new(:virtual_path => %q{spree/shared/_products},
             <%= product.display_price %>
           </span>
 
-      <% end %>})
-
-Deface::Override.new(:virtual_path => %q{spree/shared/_products},
-                     :name => %q{add_tires_info_in_products},
-                     :replace => "code[erb-loud]:contains('link_to truncate')",
-                     :text => %q{
-     <%= link_to truncate(product.name, :length => 50), product, :class => 'info', :itemprop => "name", :title => product.name %>
-     <br> <%= product.calculate_tires(product.id) %>})
+      <% end %>
+      </li>
+    <% end %>
+  <% end %>
+  <% reset_cycle("classes") %>
+</ul>})
