e6b8162fc1cbb6d5e9ce559cb814216d3822b936
jose miguel <miguel.gamazo@galiclick.com>
2012-10-14 19:50:41 +0200

    Mas mejoras

--- app/overrides/add_about_to_header.rb
+++ app/overrides/add_about_to_header.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
                      :name => "add_about_in_header",
                      :insert_bottom => "#main-nav-bar",
-                     :text => "<li class='<%= (request.fullpath.gsub('//','/') == '/about') ? 'current' : 'not'%>'><%= link_to t('spree.about'), about_index_url  %></li>")
+                     :text => "<li class='<%= (request.fullpath.gsub('//','/') == '/about') ? 'current' : 'not'%>'><%= link_to t('about'), about_index_url  %></li>")
