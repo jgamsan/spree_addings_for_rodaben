86f208877ac2deec7e96fd6773304529c5195193
jose miguel <miguel.gamazo@galiclick.com>
2012-10-21 11:25:43 +0200

    Mas mejoras

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -4,9 +4,14 @@ Spree::HomeController.class_eval do
   respond_to :html
 
   def index
+    session[:vehicle] = "car" if session[:vehicle] == nil
     @searcher = Spree::Config.searcher_class.new(params.merge(:in_offert => true))
     @products = @searcher.retrieve_products
     respond_with(@products)
+  end
+
+  def choose_vehicle
+
   end
 
 end
