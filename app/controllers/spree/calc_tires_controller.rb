b6e3a24e8da3ab8fb531835b9749d0539f33b90f
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-10-29 22:56:17 +0100

    mas mejoras

--- app/controllers/spree/calc_tires_controller.rb
+++ app/controllers/spree/calc_tires_controller.rb
@@ -1,7 +1,10 @@
 module Spree
   class CalcTiresController < BaseController
     def calc_equivalents
-      @equivalents = 25
+      ancho = Spree::TireWidth.find(params[:width]).name
+      perfil = Spree::TireSerial.find(params[:serial]).name
+      llanta = Spree::TireInnertube.find(params[:innertube]).name
+      @equivalents = (((ancho.to_f/ 25.4) * (perfil.to_f/100) * 2) + llanta.to_f) * 25.4
       respond_to do |format|
         format.js
       end
