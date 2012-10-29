01ff05694a75c5fdfa5d3d9fe280f1d25b8ce42a
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-10-29 23:59:08 +0100

    mas mejoras

--- app/controllers/spree/calc_tires_controller.rb
+++ app/controllers/spree/calc_tires_controller.rb
@@ -1,13 +1,39 @@
 module Spree
   class CalcTiresController < BaseController
     def calc_equivalents
+      @equivalentes = []
       ancho = Spree::TireWidth.find(params[:width]).name
       perfil = Spree::TireSerial.find(params[:serial]).name
       llanta = Spree::TireInnertube.find(params[:innertube]).name
-      @equivalents = (((ancho.to_f/ 25.4) * (perfil.to_f/100) * 2) + llanta.to_f) * 25.4
+      @diametro = calculo_diametro(ancho, perfil, llanta)
+      minimum = @diametro * 0.97
+      maximum = @diametro * 1.03
+      ta = Spree::TireWidth.in_mm(true).map {|c| c.name}
+      tp = Spree::TireSerial.in_mm(true).map {|c| c.name}
+      tl = Spree::TireInnertube.in_mm(true).map {|c| c.name}
+      ta.each do |width|
+        tp.each do |serial|
+          tl.each do |innertube|
+            parcial = calculo_diametro(width, serial, innertube)
+            if (parcial <= maximum) && parcial >= minimum)
+              neumatico = ta + "/" + tp + "R" + tl
+              @equivalentes << [neumatico, neumatico]
+            end
+          end
+        end
+      end
+
+
       respond_to do |format|
         format.js
       end
     end
+
+    private
+
+    def calculo_diametro(ancho, perfil, llanta)
+      ((((ancho.to_f / 25.4) * (perfil.to_f/100) * 2) + llanta.to_f) * 25.4).round(2)
+    end
+
   end
 end
