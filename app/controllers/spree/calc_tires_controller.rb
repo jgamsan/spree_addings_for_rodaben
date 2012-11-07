dca80068d0d27cdacb798701b03e8484f4867852
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-11-07 19:33:22 +0100

    Mas mejoras

--- app/controllers/spree/calc_tires_controller.rb
+++ app/controllers/spree/calc_tires_controller.rb
@@ -1,9 +1,17 @@
 module Spree
   class CalcTiresController < BaseController
     helper 'spree/products'
-
+    respond_to :html
     def index
-
+      rueda = params[:lista]
+      rueda =~ %r{(\d+)(?:/|:)(\d+)(?:\D|:)(\d+)}
+      r = [$1,$2,$3]
+      ancho = Spree::TireWidth.find_by_name(r[0]).id
+      serial = Spree::TireSerial.find_by_name(r[1]).id
+      llanta = Spree::TireInnertube.find_by_name(r[2]).id
+      @searcher = Spree::Config.searcher_class.new(params.merge(:tire_width_id => ancho, :tire_serial_id => serial, :tire_innertube_id => llanta))
+      @products = @searcher.retrieve_products
+      respond_with(@products)
     end
 
     def calc_equivalents
