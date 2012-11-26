df135a9165af38359010611f3881dffce445fbe3
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-11-26 16:25:27 +0100

    mas mejoras

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -1,4 +1,5 @@
 Spree::Taxon.class_eval do
+  layout "contenido"
   scope :by_car,
     joins(:products).where("spree_products.id IN (SELECT spree_products.id FROM spree_products INNER JOIN spree_products_taxons ON spree_products_taxons.product_id = spree_products.id INNER JOIN spree_taxons ON spree_taxons.id = spree_products_taxons.taxon_id WHERE (spree_taxons.id IN (4, 5, 6, 7, 8))) and spree_taxons.parent_id =  2").order("spree_taxons.name").group("spree_taxons.id")
 
