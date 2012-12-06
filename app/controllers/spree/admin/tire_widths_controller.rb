09110a1c483472beddbb9f4110f2720c02f33256
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-12-06 13:21:33 +0100

    mas mejoras

--- app/controllers/spree/admin/tire_widths_controller.rb
+++ app/controllers/spree/admin/tire_widths_controller.rb
@@ -3,10 +3,10 @@ module Spree
     class TireWidthsController < ResourceController
 
       def index
+        params[:q] ||= {}
         params[:q] ||= "name asc"
         @search = Spree::TireWidth.ransack(params[:q])
         @tire_widths = @search.result.page(params[:page]).per(10)
-
       end
 
     end
