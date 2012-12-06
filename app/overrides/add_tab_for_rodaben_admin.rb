5697f678786f4423a9167dcdf25c29dc756a9c9c
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-12-06 09:48:18 +0100

    mas mejoras

--- app/overrides/add_tab_for_rodaben_admin.rb
+++ app/overrides/add_tab_for_rodaben_admin.rb
@@ -3,4 +3,4 @@ Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :insert_bottom => "[data-hook='admin_tabs']",
                      :text => "<%= tab :tire_settings, :tire_widths,
                      :tire_innertubes, :tire_serials, :tire_speed_codes,
-                      :workshops, :load_massive_images, :label => 'Gestion Neumaticos' %>")
+                      :workshops, :load_massive_images, :label => 'Gestion Neumaticos', :icon => 'icon-truck' %>")
