6d85ee391e9a060416b565bbf198687d57f8bbb9
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-12-12 17:56:20 +0100

    Initial commit

--- app/controllers/spree/calc_tires_controller.rb
+++ app/controllers/spree/calc_tires_controller.rb
@@ -4,13 +4,19 @@ module Spree
     helper 'spree/products'
     respond_to :html
     def index
-      rueda = params[:lista]
-      rueda =~ %r{(\d+)(?:/|:)(\d+)(?:\D|:)(\d+)}
-      r = [$1,$2,$3]
-      ancho = Spree::TireWidth.find_by_name(r[0]).id
-      serial = Spree::TireSerial.find_by_name(r[1]).id
-      llanta = Spree::TireInnertube.find_by_name(r[2]).id
-      @searcher = Spree::Config.searcher_class.new(params.merge(:tire_width_id => ancho, :tire_serial_id => serial, :tire_innertube_id => llanta))
+      if params.has_key?(:lista)
+        rueda = params[:lista]
+        rueda =~ %r{(\d+)(?:/|:)(\d+)(?:\D|:)(\d+)}
+        r = [$1,$2,$3]
+        ancho = Spree::TireWidth.find_by_name(r[0]).id
+        serial = Spree::TireSerial.find_by_name(r[1]).id
+        llanta = Spree::TireInnertube.find_by_name(r[2]).id
+        @searcher = Spree::Config.searcher_class.new(params.merge(:tire_width_id => ancho, :tire_serial_id => serial, :tire_innertube_id => llanta))
+      else
+        innertube = params[:llanta]
+        llanta = Spree::TireInnertube.find_by_name(innertube).id
+        @searcher = Spree::Config.searcher_class.new(params.merge(:tire_innertube_id => llanta))            
+      end
       @products = @searcher.retrieve_products
       respond_with(@products)
     end
