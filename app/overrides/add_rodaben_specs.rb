b8273d9c82c0d7a6cb64e18640a185791d000d7d
jose miguel <miguel.gamazo@galiclick.com>
2012-12-20 20:33:43 +0100

    Mas mejoras

--- app/overrides/add_rodaben_specs.rb
+++ app/overrides/add_rodaben_specs.rb
@@ -1,4 +1,11 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :name => "add_rodaben_specs",
                      :insert_bottom => "#shipping_specs",
-                     :text => %q{ })
+                     :text => %q{
+        <%= f.field_container :tire_green_rate do %>
+  <%= f.label :tire_green_rate_id, t(:green_rate) %><br />
+  <%= f.collection_select(:tire_green_rate_id, Spree::TireGreenRate.order('cat'), :id, :cat, { :include_blank => true }, { :class => 'select2', :style => "width:120px;" }) %>
+  <%= f.error_message_on :tire_green_rate %>
+<% end -%>
+
+                      })
