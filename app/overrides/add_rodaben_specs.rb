d81caba8e04c289e15d8f116087edb0a1173a779
jose miguel <miguel.gamazo@galiclick.com>
2012-12-20 20:14:32 +0100

    Mas mejoras

--- app/overrides/add_rodaben_specs.rb
+++ app/overrides/add_rodaben_specs.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :name => "add_rodaben_specs",
                      :insert_bottom => "#shipping_specs",
-                     :text => %q{ <%= render :partial => "spree/admin/products/tires", :locals => {:f => f} %> })
+                     :text => %q{ })
