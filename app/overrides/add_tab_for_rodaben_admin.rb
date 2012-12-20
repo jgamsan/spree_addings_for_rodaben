3dd55d4922561a533c7c6bde0513381bad4be607
jose miguel <miguel.gamazo@galiclick.com>
2012-12-20 17:39:20 +0100

    Mas mejoras

--- app/overrides/add_tab_for_rodaben_admin.rb
+++ app/overrides/add_tab_for_rodaben_admin.rb
@@ -2,5 +2,5 @@ Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "add_tab_for_rodaben_admin",
                      :insert_bottom => "[data-hook='admin_tabs']",
                      :text => "<%= tab :tire_settings, :tire_widths,
-                     :tire_innertubes, :tire_serials, :tire_speed_codes, :tire_load_codes, :tire_green_taxes,
+                     :tire_innertubes, :tire_serials, :tire_speed_codes, :tire_load_codes, :tire_green_rates,
                      :workshops, :load_massive_images, :label => 'Gestion Neumaticos', :icon => 'icon-truck', :url => admin_tire_settings_path %>")
