2f5b2a073d42380dc7e2b769700ecba40b5fa83c
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2013-01-17 00:05:30 +0100

    Mas Modificaciones

--- lib/paperclip_processors/greenlabel.rb
+++ lib/paperclip_processors/greenlabel.rb
@@ -9,6 +9,8 @@ module Paperclip
       @current_format = File.extname(@file.path)
       @basename = File.basename(@file.path, @current_format)
       @position = "center"
+      @fuel_options = Hash["A", "-14-55", "B", "-13-33", "C", "-13-11", "D", "-13+11", "E", "-13+33", "F", "-13+55", "G", "-13+77"]
+      @wet_options = Hash["A", "+103-53", "B", "+103-31", "C", "+103-9", "D", "+103+13", "E", "+103+35", "F", "+103+56", "G", "+103+78"]
     end
     
     def make
@@ -16,12 +18,16 @@ module Paperclip
       dst.binmode 
       
       command = "composite"
+      comman2 = "convert"
       params = "-gravity #{@position} -geometry -13-33 #{overfile} #{fromfile} #{tofile(dst)}"
       params2 = "-gravity #{@position} -geometry +103+13 #{Rails.root}/app/assets/images/d.jpg #{tofile(dst)} #{tofile(dst)}"
-      
+      params3 = "-gravity #{@position} -geometry -30+165 #{Rails.root}/app/assets/images/emision_ruido_2.jpg #{tofile(dst)} #{tofile(dst)}"
+      params4 = "-gravity center -pointsize 30 -draw 'text 60,168 "73"' -font monospace -fill '#ffffff' #{tofile(dst)} #{tofile(dst)}"
       begin
         success = Paperclip.run(command, params)
         sucess = Paperclip.run(command, params2)
+        sucess = Paperclip.run(command, params3)
+        sucess = Paperclip.run(command2, params4)
       rescue Exception => e
         raise e, "Hubo un error en el proceso de creacion etiqueta CEE"
       end
