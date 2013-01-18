9633a1213b35f7ae4fba2d1739b30871bfeaeb61
jose miguel <miguel.gamazo@galiclick.com>
2013-01-18 19:30:47 +0100

    Mas mejoras

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,38 +1,36 @@
 Spree::Image.class_eval do
   Spree::Image.attachment_definitions[:attachment][:styles].merge!(
-      :ceelabel => {:format => :jpg, :processors => [:greenlabel]}
+      :ceelabel => {:format => :jpg, :processors => [:greenlabel]},
+      :watermark => {:processors => [:thumbnail, :offertmark],
+                     :styles => {
+                        :mini => {
+                          :geometry => '48x48>',
+                          :offertmark_path => "#{Rails.root.to_s}/public/images/offertmarks/tiny.png",
+                          :offertmark_position => "SouthWest",
+                          :format => :png,
+                        },
+                        :small => {
+                          :geometry => '100x100>',
+                          :offertmark_path => "#{Rails.root.to_s}/public/images/offertmarks/small.png",
+                          :offertmark_position => "SouthWest",
+                          :format => :png,
+                        },
+                        :product => {
+                          :geometry => '240x240>',
+                          :offertmark_path => "#{Rails.root.to_s}/public/images/offertmarks/product.png",
+                          :offertmark_position => "SouthWest",
+                          :format => :png,
+                        },
+                        :large => {
+                          :geometry => '600x600>',
+                          :offertmark_path => "#{Rails.root.to_s}/public/images/offertmarks/large.png",
+                          :offertmark_position => "SouthWest",
+                          :format => :png,
+                        },
+                      },
+                     :default_style => :product,
+                     :url => "/spree/products/:id/:style/:basename.:extension",
+                     :path => ":rails_root/public/spree/products/:id/:style/:basename.:extension"
+      }
   )
-  has_attached_file(
-    :attachment,
-    :processors => [:thumbnail, :offertmark],
-    :styles => {
-      :mini => {
-        :geometry => '48x48>',
-        :offertmark_path => "#{Rails.root.to_s}/public/images/offertmarks/tiny.png",
-        :offertmark_position => "SouthWest",
-        :format => :png,
-      },
-      :small => {
-        :geometry => '100x100>',
-        :offertmark_path => "#{Rails.root.to_s}/public/images/offertmarks/small.png",
-        :offertmark_position => "SouthWest",
-        :format => :png,
-      },
-      :product => {
-        :geometry => '240x240>',
-        :offertmark_path => "#{Rails.root.to_s}/public/images/offertmarks/product.png",
-        :offertmark_position => "SouthWest",
-        :format => :png,
-      },
-      :large => {
-        :geometry => '600x600>',
-        :offertmark_path => "#{Rails.root.to_s}/public/images/offertmarks/large.png",
-        :offertmark_position => "SouthWest",
-        :format => :png,
-      },
-    },
-    :default_style => :product,
-    :url => "/spree/products/:id/:style/:basename.:extension",
-    :path => ":rails_root/public/spree/products/:id/:style/:basename.:extension"
-    )
 end
