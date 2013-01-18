2004e2f36d7c5f4f57b359ff7860cf973883253e
jose miguel <miguel.gamazo@galiclick.com>
2013-01-18 20:01:57 +0100

    Mas mejoras

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Image.class_eval do
   Spree::Image.attachment_definitions[:attachment][:styles].merge!(
       :ceelabel => {:format => :jpg, :processors => [:greenlabel]},
-      :watermark => {:processors => [:thumbnail, :offertmark],
+      :offertmark => {:processors => [:thumbnail, :offertmark],
                      :styles => {
                         :mini => {
                           :geometry => '48x48>',
