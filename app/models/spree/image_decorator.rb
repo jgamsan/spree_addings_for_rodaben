d2796419445b75fd9a9cd11aa400a934a8b47bbb
jose miguel <miguel.gamazo@galiclick.com>
2013-01-19 12:17:46 +0100

    Mas mejoras

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,32 +1,7 @@
 Spree::Image.class_eval do
   Spree::Image.attachment_definitions[:attachment][:styles].merge!(
       :ceelabel => {:format => :jpg, :processors => [:greenlabel]},
-      :offertmark => {:processors => [:offertmark],
-                     :styles => {
-                        :mini => {
-                          :geometry => '48x48>',
-                          :offertmark_position => "SouthWest",
-                          :format => :png
-                        },
-                        :small => {
-                          :geometry => '100x100>',
-                          :offertmark_position => "SouthWest",
-                          :format => :png
-                        },
-                        :product => {
-                          :geometry => '240x240>',
-                          :offertmark_position => "SouthWest",
-                          :format => :png
-                        },
-                        :large => {
-                          :geometry => '600x600>',
-                          :offertmark_position => "SouthWest",
-                          :format => :png
-                        },
-                      },
-                     :default_style => :product,
-                     :url => "/spree/products/:id/:style/:basename.:extension",
-                     :path => ":rails_root/public/spree/products/:id/:style/:basename.:extension"
-      }
+      :offertmark => {:format => :jpg,, :processors => [:offertmark]},
+      :newmark => {:format => :jpg,, :processors => [:newmark]}
   )
 end
--- lib/paperclip_processors/greenlabel.rb
+++ lib/paperclip_processors/greenlabel.rb
@@ -9,9 +9,7 @@ module Paperclip
       @current_format = File.extname(@file.path)
       @basename = File.basename(@file.path, @current_format)
       @position = "center"
-      @fuel_options = Hash["A", "-14-55", "B", "-13-33", "C", "-13-11", "D", "-13+11", "E", "-13+33", "F", "-13+55", "G", "-13+77"]
-      @wet_options = Hash["A", "+103-53", "B", "+103-31", "C", "+103-9", "D", "+103+13", "E", "+103+35", "F", "+103+56", "G", "+103+78"]
-      @image = MiniMagick::Image.open("#{Rails.root}/app/assets/images/base_etiqueta.jpg")
+      @image = "#{Rails.root}/app/assets/images/base_etiqueta.jpg"
     end
 
     def make
@@ -19,41 +17,11 @@ module Paperclip
       dst.binmode
 
       begin
-        result = @image.composite(MiniMagick::Image.open(overfile, "jpg")) do |c|
-          c.gravity "center"
-          c.geometry "-13-33"
-        end
-        result = result.composite(MiniMagick::Image.open("#{Rails.root}/app/assets/images/d.jpg", "jpg")) do |c|
-          c.gravity "center"
-          c.geometry "+103+13"
-        end
-        result = result.composite(MiniMagick::Image.open("#{Rails.root}/app/assets/images/emision_ruido_3.jpg", "jpg")) do |c|
-          c.gravity "center"
-          c.geometry "-30+165"
-        end
-        result.combine_options do |c|
-          c.gravity "center"
-          c.pointsize '30'
-          c.draw "text 60,168 '73'"
-          c.font 'arial'
-          c.fill "#FFFFFF"
-        end
+        dst = @image
       rescue Exception => e
         raise e, "Hubo un error en el proceso de creacion etiqueta CEE"
       end
-      result.write dst
-    end
-
-    def basefile
-      "#{Rails.root}/app/assets/images/base_etiqueta.jpg"
-    end
-
-    def overfile
-      "#{Rails.root}/app/assets/images/b.jpg"
-    end
-
-    def tofile(destination)
-      "\"#{ File.expand_path(destination.path) }[0]\""
+      dst
     end
   end
 
@@ -63,7 +31,7 @@ module Paperclip
       @file = file
       @whiny = options[:whiny].nil? ? true : options[:whiny]
       @format = options[:format]
-      @position = options[:offertmark_position].nil? ? "SouthEast" : options[:offertmark_position]
+      @position = "NorthWest"
 
       @current_format = File.extname(@file.path)
       @basename = File.basename(@file.path, @current_format)
@@ -75,12 +43,50 @@ module Paperclip
       dst.binmode
 
       command = "composite"
-      params = "-gravity #{@position} #{Rails.root}/app/assets/images/label_red.png #{fromfile} #{tofile(dst)}"
+      params = "-gravity #{@position} #{Rails.root}/app/assets/images/yellow.png #{fromfile} #{tofile(dst)}"
 
       begin
         success = Paperclip.run(command, params)
       rescue Cocaine::CommandLineError => ex
-        raise ex, "There was an error processing the watermark for #{@basename}" if @whiny
+        raise ex, "There was an error processing the offertmark for #{@basename}" if @whiny
+      end
+
+      dst
+    end
+
+    def fromfile
+      "\"#{ File.expand_path(@file.path) }[0]\""
+    end
+
+    def tofile(destination)
+      "\"#{ File.expand_path(destination.path) }[0]\""
+    end
+
+  end
+
+  class Newmark < Processor
+    attr_accessor :format, :whiny, :position
+    def initialize file, options = {}, attachment = nil
+      @file = file
+      @whiny = options[:whiny].nil? ? true : options[:whiny]
+      @format = options[:format]
+      @position = "NorthWest"
+
+      @current_format = File.extname(@file.path)
+      @basename = File.basename(@file.path, @current_format)
+    end
+
+    def make
+      dst = Tempfile.new([@basename, @format].compact.join("."))
+      dst.binmode
+
+      command = "composite"
+      params = "-gravity #{@position} #{Rails.root}/app/assets/images/blue.png #{fromfile} #{tofile(dst)}"
+
+      begin
+        success = Paperclip.run(command, params)
+      rescue Cocaine::CommandLineError => ex
+        raise ex, "There was an error processing the newmark for #{@basename}" if @whiny
       end
 
       dst
