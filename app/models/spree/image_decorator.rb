8c6d96c7b6d3954373f76bdb40b0707ef30295a1
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2013-01-29 22:27:00 +0100

    Mas Modificaciones

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -4,4 +4,13 @@ Spree::Image.class_eval do
       :offertmark => {:format => :png, :processors => [:offertmark]},
       :newmark => {:format => :png, :processors => [:newmark]}
   )
+
+  after_save :check_cee_label, :on => :create
+
+  def check_cee_label
+    variante = Spree::Variant.find(self.viewable_id)
+    if variante.images.count == 1
+      update_cee_label_imagen
+    end
+  end
 end
