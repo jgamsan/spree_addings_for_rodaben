32bc57cf6234e69f3c930c0d49911759cc27c71e
jose miguel <miguel.gamazo@galiclick.com>
2013-02-04 21:28:36 +0100

    Mas mejoras

--- app/models/spree/tire_setting.rb
+++ app/models/spree/tire_setting.rb
@@ -1,4 +1,4 @@
 class Spree::TireSetting < ActiveRecord::Base
-  attr_accessor :image, :tyre, :tires_massive, :width
-  attr_accessible :image, :tyre, :tires_massive, :width
+  attr_accessor :image, :tyre, :tires_massive, :width, :green_rate
+  attr_accessible :image, :tyre, :tires_massive, :width, :green_rate
 end
