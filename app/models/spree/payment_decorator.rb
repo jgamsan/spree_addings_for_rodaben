2dbc9034a0949c814bc80fa5cf5311fa3bb95bc3
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2013-02-27 17:08:52 +0100

    Mas mejoras

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -5,9 +5,10 @@ Spree::Payment.class_eval do
     if (self.state_changed? || self.state == "completed" || self.payment.payment_method_id == 2)
       begin
         @suppliers = Spree::Supplier.all
+        @order = self.order
         @suppliers.each do |supplier|
           @line_items = Spree::LineItem.joins(:variant, :product).where("spree_products.supplier_id = ? and spree_line_items.order_id = ?", supplier.id, self.order_id)
-          Spree::NotifyMailer.send_email_to_provider(self.order, @line_items).deliver unless @line_items.empty?
+          Spree::NotifyMailer.send_email_to_provider(@order, @line_items).deliver unless @line_items.empty?
         end
       rescue Exception => e
         logger.error("#{e.class.name}: #{e.message}")
