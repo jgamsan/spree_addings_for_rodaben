85c1ba7ca1fd60331530e62e40dc492539d5e7ec
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2013-02-28 23:05:06 +0100

    Mas mejoras

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -3,16 +3,12 @@ Spree::Order.class_eval do
   attr_accessible :workshop_id
   belongs_to :workshop
 
-  self.state_machine.after_transition :to => :complete, :do => :check_emails_to_send
+  #self.state_machine.after_transition :to => :complete, :do => :check_emails_to_send
 
   def check_emails_to_send
     deliver_order_company_provider_email unless payment_by_transfer?
     deliver_order_workshop_email unless self.workshop_id.nil?
   end
-
-  # def request!
-  #   deliver_order_company_provider_email
-  # end
 
   def deliver_order_company_provider_email
     begin
