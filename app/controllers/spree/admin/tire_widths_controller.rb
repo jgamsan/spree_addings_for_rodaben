f6c9fdbc51629c750e6113e0d672d753a6c595d6
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2013-03-29 20:52:28 +0100

    Mas mejoras en motos

--- app/controllers/spree/admin/tire_widths_controller.rb
+++ app/controllers/spree/admin/tire_widths_controller.rb
@@ -7,6 +7,13 @@ module Spree
         params[:q] ||= "name asc"
         @search = Spree::TireWidth.ransack(params[:q])
         @tire_widths = @search.result.page(params[:page]).per(10)
+        respond_to do |format|
+          format.html # index.html.erb
+          format.json { 
+            @tire_widths = Spree::TireWidth.all
+            render json: @tire_widths 
+          }
+        end
       end
 
     end
